b62758ca0fb676bf7268c4a1dd23abaa1f31f409
vitorsilvalima <vitor.lima2@fatec.sp.gov.br>
2017-01-17 14:55:55 -0200

    adding sortable features back to the project

--- src/app/services/contants.js
+++ src/app/services/contants.js
@@ -1,8 +1,8 @@
 (function () {
 	'use strict';
 	var contants= angular.module('constants',[])
-	//.constant('ENDPOINT_API', 'http://localhost:4000/api/')
-	.constant('ENDPOINT_API', 'http://187.3.39.80:8080/api/')
+	.constant('ENDPOINT_API', 'http://localhost:4000/api/')
+	//.constant('ENDPOINT_API', 'http://187.3.39.80:8080/api/')
 	//.constant('ENDPOINT_API', 'http://192.168.0.50:8080/api/')
 	//.constant('ENDPOINT_URL', 'http://localhost:4000')
 	//.constant('ENDPOINT_API', 'http://191.182.25.177:8080/api/')
